4be7d34158e88911599bd408f502cefb6be2c846
Joshua Leonard <joshualeonard@Joshuas-Mac-mini.local>
2024-03-16 11:53:53 -0400

    added go docs

--- goro.go
+++ goro.go
@@ -1,6 +1,8 @@
+// Package goro provides a method to add 2 integers
 package goro
 
-// Add function
+// Add function allows you to add 2 integers and get a resulting integer
+// There is a link on adding [Math Addition]: https://www.mathsisfun.com/numbers/addition.html
 func Add(num1, num2 int) int {
 	return num1 + num2
 }
