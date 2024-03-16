27265ac8f750cf56947fa513e3c9956544067a43
Joshua Leonard <joshualeonard@Joshuas-Mac-mini.local>
2024-03-16 12:10:54 -0400

    updated to take different types

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/JoshLeonard/goro
 
 go 1.22.0
+
+require golang.org/x/exp v0.0.0-20240314144324-c7f7c6466f7f
--- goro.go
+++ goro.go
@@ -1,8 +1,23 @@
 // Package goro provides a method to add 2 integers
 package goro
 
+import (
+	"golang.org/x/exp/constraints"
+)
+
+type Number interface {
+	constraints.Integer
+	constraints.Float
+}
+
 // Add function allows you to add 2 integers and get a resulting integer
 // There is a link on adding [Math Addition]: https://www.mathsisfun.com/numbers/addition.html
-func Add(num1, num2 int) int {
-	return num1 + num2
+func Add(num1, num2 Number) Number {
+	switch num1.(type) {
+	case int:
+		return num1.(int) + num2.(int)
+	case float64:
+		return num1.(float64) + num2.(float64)
+	}
+	return 0
 }
